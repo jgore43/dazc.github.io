522aa5e668542914b22bac6d797dfd899748b89f
DazCo <deonic@live.com>
2015-09-26 18:28:23 -0400

    Back on track

--- ytr/script.js
+++ ytr/script.js
@@ -4,13 +4,13 @@ $(document).ready(function() {
 	XZcItivrKDg
 	0SDlb_TU9-o
 */
-	var yturl = "XZcItivrKDg";
+	var ytid = "U9fXPOCfqqE";
 	
-	$('#ytarcontainer').append("<div id=\"ytar\"><br><iframe width=\"100%\" height=\"160\" src=\"https://www.youtube.com/embed/" + yturl + "?version=3&amp;loop=1&amp;playlist=" + yturl + "&amp;autoplay=1" + "\" frameborder=\"0\" allowfullscreen></iframe></div>");
+	$('#ytarcontainer').append("<div id=\"ytar\"><br><iframe width=\"100%\" height=\"160\" src=\"https://www.youtube.com/embed/" + ytid + "?version=3&amp;loop=1&amp;playlist=" + ytid + "&amp;autoplay=1" + "\" frameborder=\"0\" allowfullscreen></iframe></div>");
 	
 	$('button').click(function() {
-		yturl = $("input[name=YTautoreplay]").val();
+		ytid = $("input[name=YTautoreplay]").val();
 		$('#ytar').remove();
-		$('#ytarcontainer').append("<div id=\"ytar\"><br><iframe width=\"100%\" height=\"160\" src=\"https://www.youtube.com/embed/" + yturl + "?version=3&amp;loop=1&amp;playlist=" + yturl + "&amp;autoplay=1" + "\" frameborder=\"0\" allowfullscreen></iframe></div>");
+		$('#ytarcontainer').append("<div id=\"ytar\"><br><iframe width=\"100%\" height=\"160\" src=\"https://www.youtube.com/embed/" + ytid + "?version=3&amp;loop=1&amp;playlist=" + ytid + "&amp;autoplay=1" + "\" frameborder=\"0\" allowfullscreen></iframe></div>");
 	});
 });
