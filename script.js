7e610499eabdcb2f6c741be80edc525c7e9b2343
DazCo <deonic@live.com>
2015-09-26 17:02:41 -0400

    Scripting

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	XZcItivrKDg
 	*/
 	
-		$('#ytar').append("<br><iframe width=\"100%\" height=\"80\" src=\"https://www.youtube.com/embed/" + "U9fXPOCfqqE" + "?version=3&amp;loop=1&amp;playlist=" + "U9fXPOCfqqE" + "&amp;autoplay=1 frameborder=\"0\" allowfullscreen></iframe>");
+	$('#ytar').append("<br><iframe width=\"100%\" height=\"80\" src=\"https://www.youtube.com/embed/" + "U9fXPOCfqqE" + "?version=3&amp;loop=1&amp;playlist=" + "U9fXPOCfqqE" + "&amp;autoplay=1 frameborder=\"0\" allowfullscreen></iframe>");
 		
 	$('button').click(function() {
 		var yturl = $("input[name=YTautoreplay]").val();
---
+++ ytr/README.md
@@ -0,0 +1,9 @@
+READ ME files exist for completion for proof of concept
+Concept: Given a youtube url, autoplay an infinite loop of that video
+
+TODO:
+# Input
+	Accept full or partial address identifying 11 characters after "v="
+	Accept entire playlist
+# Nonfunctional
+	Build a history list
