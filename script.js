05cb05d6ece4f2e645527cc3ddbdbd5c16322630
DazCo <deonic@live.com>
2015-09-26 13:58:33 -0400

    Scripting

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
     /* U9fXPOCfqqE */
 	$('button').click(function() {
 		var yturl = $("input[name=YTautoreplay]").val();
-		$('#ytar').append("<br><iframe width=\"100%\" height=\"80\" src=\"https://www.youtube.com/embed/" + yturl + "?version=3&amp;loop=1&amp;playlist=" + yturl + "&amp;autoplay=1 frameborder=0 allowfullscreen></iframe>");
+		$('#ytar').append("<br><iframe width=\"100%\" height=\"80\" src=\"https://www.youtube.com/embed/" + yturl + "?version=3&amp;loop=1&amp;playlist=" + yturl + "&amp;autoplay=1 frameborder='0' allowfullscreen></iframe>");
 	});
 });
