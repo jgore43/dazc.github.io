12750584efd733fff69bad1956d17c24cd281d48
DazCo <deonic@live.com>
2015-09-26 15:51:21 -0400

    Scripting

--- p00/script.js
+++ p00/script.js
@@ -12,3 +12,11 @@ $(document).ready(function() {
 		$('#ytarcontainer').append("<div id=\"ytar\"><br><iframe width=\"100%\" height=\"80\" src=\"https://www.youtube.com/embed/" + yturl + "?version=3&amp;loop=1&amp;playlist=" + yturl + "&amp;autoplay=1 frameborder=\"0\" allowfullscreen></iframe></div>");
 	});
 });
+
+<script>
+$('button').click(function() {
+	var yturl = $("input[name=YTautoreplay]").val();
+	$('#ytar').remove();
+	$('#ytarcontainer').append("<div id=\"ytar\"><br><iframe width=\"100%\" height=\"80\" src=\"https://www.youtube.com/embed/" + yturl + "?version=3&amp;loop=1&amp;playlist=" + yturl + "&amp;autoplay=1 frameborder=\"0\" allowfullscreen></iframe></div>");
+});
+</script>
