147becc4448350719f2d7ae7e9aafbce69e20c91
DazCo <deonic@live.com>
2015-09-26 17:55:38 -0400

    Scripting

--- ytr/script.js
+++ ytr/script.js
@@ -14,4 +14,9 @@ $(document).ready(function() {
 		$('#ytar').remove();
 		$('#ytarcontainer').append(ytdiv);
 	});
+	$('#testcases li').click(function(){
+		yturl = $(this).text();
+		$('#ytar').remove();
+		$('#ytarcontainer').append(ytdiv);
+	});
 });
