df646f206b262aa80731b89247970b5691274788
DazCo <deonic@live.com>
2015-09-26 18:49:59 -0400

    Identifying probable err

--- ytr/script.js
+++ ytr/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 	0SDlb_TU9-o
 */
 	var ytid = "0SDlb_TU9-o";
-	var ytdiv = function(id) { return "<div id=\"ytar\"><br><iframe width=\"100%\" height=\"160\" src=\"https://www.youtube.com/embed/" + id + "?version=3&amp;loop=1&amp;playlist=" + id + "&amp;autoplay=1" + "\" frameborder=\"0\" allowfullscreen></iframe></div>"; }
+	var ytdiv = function(id) { return "<div id=\"ytar\"><iframe width=\"100%\" height=\"160\" src=\"https://www.youtube.com/embed/" + id + "?version=3&amp;loop=1&amp;playlist=" + id + "&amp;autoplay=1" + "\" frameborder=\"0\" allowfullscreen></iframe></div>"; }
 	
 	$('#ytarcontainer').append(ytdiv(ytid));
 	
