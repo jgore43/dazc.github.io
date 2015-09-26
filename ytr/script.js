f1e851a9b263eee5a0aef6fd41f50af6791d7db9
DazCo <deonic@live.com>
2015-09-26 18:44:50 -0400

    Identifying probable err

--- ytr/script.js
+++ ytr/script.js
@@ -5,13 +5,19 @@ $(document).ready(function() {
 	0SDlb_TU9-o
 */
 	var ytid = "0SDlb_TU9-o";
-	var ytaddr = function(id) { return "<div id=\"ytar\"><br><iframe width=\"100%\" height=\"160\" src=\"https://www.youtube.com/embed/" + id + "?version=3&amp;loop=1&amp;playlist=" + id + "&amp;autoplay=1" + "\" frameborder=\"0\" allowfullscreen></iframe></div>"; }
+	var ytdiv = function(id) { return "<div id=\"ytar\"><br><iframe width=\"100%\" height=\"160\" src=\"https://www.youtube.com/embed/" + id + "?version=3&amp;loop=1&amp;playlist=" + id + "&amp;autoplay=1" + "\" frameborder=\"0\" allowfullscreen></iframe></div>"; }
 	
-	$('#ytarcontainer').append(ytaddr(ytid));
+	$('#ytarcontainer').append(ytdiv(ytid));
 	
 	$('button').click(function() {
 		ytid = $("input[name=YTautoreplay]").val();
 		$('#ytar').remove();
-		$('#ytarcontainer').append(ytaddr(ytid));
+		$('#ytarcontainer').append(ytdiv(ytid));
+	});
+	
+	$('li').click(function() {
+		ytid = $(this).text(); // set name of clicked li
+		$('#ytar').remove();
+		$('#ytarcontainer').append(ytdiv(ytid));
 	});
 });
