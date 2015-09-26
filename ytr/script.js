f944e9d0d151053be3aaf9786cc4a39ac94bad65
DazCo <deonic@live.com>
2015-09-26 18:00:51 -0400

    Scripting

--- ytr/script.js
+++ ytr/script.js
@@ -5,12 +5,18 @@ $(document).ready(function() {
 	0SDlb_TU9-o
 */
 	var ytid = "U9fXPOCfqqE";
-	var ytdiv = "<div id=\"ytar\"><br><iframe width=\"100%\" height=\"160\" src=\"https://www.youtube.com/embed/" + yturl + "?version=3&amp;loop=1&amp;playlist=" + ytid + "&amp;autoplay=1" + "\" frameborder=\"0\" allowfullscreen></iframe></div>";
+	var ytdiv = "<div id=\"ytar\"><br><iframe width=\"100%\" height=\"160\" src=\"https://www.youtube.com/embed/" + ytid + "?version=3&amp;loop=1&amp;playlist=" + ytid + "&amp;autoplay=1" + "\" frameborder=\"0\" allowfullscreen></iframe></div>";
 	
 	$('#ytarcontainer').append(ytdiv);
 	
 	$('button').click(function() {
 		yturl = $("input[name=YTautoreplay]").val();
+		$('#ytar').remove();
+		$('#ytarcontainer').append(ytdiv);
+	});
+	
+		$('li').click(function(){
+		yturl = $(this).text();
 		$('#ytar').remove();
 		$('#ytarcontainer').append(ytdiv);
 	});
